10c0d629dbdf98cf4591c1e40c1d7732954f26fc
Shreshth Tiwari <shreshth.shreya24208@gmail.com>
2024-09-07 00:37:03 +0530

    updates ig

--- i3/scripts/set_random_background.sh
+++ i3/scripts/set_random_background.sh
@@ -6,16 +6,21 @@ BG_DIR="/media/shreshth/Data/Backgrounds"
 # Number of images
 MAX_IMAGES=768
 
-# Generate a random number between 1 and MAX_IMAGES
-RANDOM_NUMBER=$(shuf -i 1-$MAX_IMAGES -n 1)
+while true; do
+  # Generate a random number between 1 and MAX_IMAGES
+  RANDOM_NUMBER=$(shuf -i 1-$MAX_IMAGES -n 1)
 
-# Construct the filename
-IMAGE_FILE="$BG_DIR/${RANDOM_NUMBER}.png"
+  # Construct the filename
+  IMAGE_FILE="$BG_DIR/${RANDOM_NUMBER}.png"
 
-# Check if the file exists
-if [ -f "$IMAGE_FILE" ]; then
-  # Set the background
-  feh --bg-scale "$IMAGE_FILE"
-else
-  echo "Background file $IMAGE_FILE does not exist."
-fi
+  # Check if the file exists
+  if [ -f "$IMAGE_FILE" ]; then
+    # Set the background
+    feh --bg-fill "$IMAGE_FILE"
+  else
+    echo "Background file $IMAGE_FILE does not exist."
+  fi
+
+  # Wait for 30 seconds before changing the background again
+  sleep 30
+done
